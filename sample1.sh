26175e35c28fdac597001ba9e02f3f6c82f0faba
vijaimca <vijaimca1983@gmail.com>
2018-12-23 18:55:56 +0530

    Hello Welcome

---
+++ sample1.sh
@@ -0,0 +1,21 @@
+!#/bin/bash -hi 
+
+
+
+	echo "Hello Welcome";
+
+ 
+git add hai.c
+ git commit -m "Hello Welcome" hai.c
+ git status
+ git  pull origin master
+ git push origin master
+
+ git add hai1.c
+ git commit -m "Hello Welcome" hai1.c
+ git status
+ git pull origin master
+ git push origin master
+
+
+
